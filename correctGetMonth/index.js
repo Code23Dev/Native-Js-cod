cc7c7c695c96d48ffe4e06c240f8be73f11e2438
leman ibrahimli <65496038+Code23Dev@users.noreply.github.com>
2021-09-03 12:36:27 +0400

    update:(correctGetMonth)

--- correctGetMonth/index.js
+++ correctGetMonth/index.js
@@ -1,5 +1,5 @@
 const monthNames = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
 const d = new Date();
-const currectMonth=monthNames[d.getMonth()]
-console.log(`The current month is  ${currectMonth} and ${monthNames.indexOf(currectMonth)+1}` );
-console.log(`${d.getDate()}.${monthNames.indexOf(currectMonth)+1}.${d.getFullYear()}`)
+const correctMonth=monthNames[d.getMonth()]
+console.log(`The correct month is  ${correctMonth} and ${monthNames.indexOf(correctMonth)+1}` );
+console.log(`${d.getDate()}.${monthNames.indexOf(correctMonth)+1}.${d.getFullYear()}`)
